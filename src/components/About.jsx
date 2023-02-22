f765610977f74496693e95a132ed3647b2be4437
Scott Dialo <scottdialo@Scotts-MacBook-Pro.local>
2023-02-22 11:01:37 -0800

    routes setup

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,40 +11,43 @@ export const About = () => {
           <h3 className="title">Little Lemon</h3>
           <p className="city">Chicago</p>
           <p>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas
-              cumque enim, nulla necessitatibus eveniet incidunt impedit dolore
-              laboriosam iusto nisi. Autem hic molestias ipsum ipsam fugit
-              repellendus excepturi odit a. Labore voluptate ea inventore
-              sapiente vel ducimus molestias cum
+            <p className="content">
+              It all started in our great grand mother kitchen, Lorem ipsum
+              dolor sit amet, consectetur adipisicing elit. Quas cumque enim,
+              nulla necessitatibus eveniet incidunt impedit dolore laboriosam
+              iusto nisi. Autem hic molestias ipsum ipsam fugit repellendus
+              excepturi odit a. Labore voluptate ea inventore sapiente vel
+              ducimus molestias
             </p>
           </p>
         </div>
         <div className="right-section">
           <img src={chef1} alt="founder1" />
-          <img src={chef2} alt="founder2" />
+          <img src={chef2} alt="founder2" className="founder2" />
         </div>
       </div>
-      <p>
-        Photo by{" "}
-        <a href="https://unsplash.com/@louishansel?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-          Louis Hansel
-        </a>{" "}
-        on{" "}
-        <a href="https://unsplash.com/s/photos/chef?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-          Unsplash
-        </a>
-      </p>
-      <p>
-        Photo by{" "}
-        <a href="https://unsplash.com/fr/@shootdelicious?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-          Eiliv Aceron
-        </a>{" "}
-        on{" "}
-        <a href="https://unsplash.com/s/photos/chef?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
-          Unsplash
-        </a>
-      </p>
+      <div className="credit">
+        <p>
+          Photo by{" "}
+          <a href="https://unsplash.com/@louishansel?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Louis Hansel
+          </a>{" "}
+          on{" "}
+          <a href="https://unsplash.com/s/photos/chef?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Unsplash
+          </a>
+        </p>
+        <p>
+          Photo by{" "}
+          <a href="https://unsplash.com/fr/@shootdelicious?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Eiliv Aceron
+          </a>{" "}
+          on{" "}
+          <a href="https://unsplash.com/s/photos/chef?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+            Unsplash
+          </a>
+        </p>
+      </div>
     </>
   );
 };
